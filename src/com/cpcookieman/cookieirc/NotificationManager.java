0d8de28d5fad7cfbd11e7616138fbcf5040f666f
Paul Hollinsky <paulhollinsky@gmail.com>
2013-05-30 14:51:41 -0400

    Fixed a bug in the classpath.

--- src/com/cpcookieman/cookieirc/NotificationManager.java
+++ src/com/cpcookieman/cookieirc/NotificationManager.java
@@ -7,19 +7,21 @@ public class NotificationManager
 {
 	public static void postNotification(String title, String subtitle, String message)
 	{
-		Main.debug("Posted Notification: " + title + ", " + subtitle + ", " + message);
 		try
 		{
 			NsUserNotificationsBridge.instance.sendNotification(title, subtitle, message, 0);
 		}
 		catch (UnsatisfiedLinkError e)
 		{
-			Main.debug("Could not load notification center libraries. Are we even on Mac?");
+			Main.debug("Could not post to the OSX Notification Center.");
 		}
+		Main.debug("Posted Notification: " + title + ", " + subtitle + ", " + message);
 	}
 	
+	// This controls the OS X dynamic library that talks to the Notification Center.
 	interface NsUserNotificationsBridge extends Library
 	{
+		// Thanks to petesh (GitHub) for this library.
 		NsUserNotificationsBridge instance = (NsUserNotificationsBridge) Native.loadLibrary("/usr/local/lib/NsUserNotificationsBridge.dylib", NsUserNotificationsBridge.class);
 		public int sendNotification(String title, String subtitle, String text, int timeoffset);
 	}
